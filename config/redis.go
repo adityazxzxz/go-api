2cba53fa153ba0c02fd7683d221367a8a5dcc4f0
Aditya <adityapratamax11@gmail.com>
2026-03-04 22:39:00 +0700

    env untuk redis

--- config/redis.go
+++ config/redis.go
@@ -2,6 +2,9 @@ package config
 
 import (
 	"context"
+	"log"
+	"os"
+	"strconv"
 
 	"github.com/redis/go-redis/v9"
 )
@@ -10,8 +13,42 @@ var Ctx = context.Background()
 var Redis *redis.Client
 
 func InitRedis() {
+
+	host := os.Getenv("REDIS_HOST")
+	if host == "" {
+		host = "localhost"
+	}
+
+	port := os.Getenv("REDIS_PORT")
+	if port == "" {
+		port = "6379"
+	}
+
+	password := os.Getenv("REDIS_PASSWORD")
+
+	dbStr := os.Getenv("REDIS_DB")
+	if dbStr == "" {
+		dbStr = "0"
+	}
+
+	db, err := strconv.Atoi(dbStr)
+	if err != nil {
+		log.Fatal("Invalid REDIS_DB value")
+	}
+
+	addr := host + ":" + port
+
 	Redis = redis.NewClient(&redis.Options{
-		Addr: "localhost:6379",
-		DB:   0,
+		Addr:     addr,
+		Password: password,
+		DB:       db,
 	})
+
+	// Test connection
+	_, err = Redis.Ping(Ctx).Result()
+	if err != nil {
+		log.Fatal("Failed to connect to Redis:", err)
+	}
+
+	log.Println("Redis connected:", addr)
 }
