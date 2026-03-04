75a9887ae6c147add4dd82a43764bab81dae5729
Aditya <adityapratamax11@gmail.com>
2026-03-04 22:40:07 +0700

    remove komen

--- main.go
+++ main.go
@@ -35,31 +35,3 @@ func main() {
 
 	router.Run(":" + os.Getenv("APP_PORT"))
 }
-
-// func setupLogging() {
-
-// 	// Pastikan folder logs ada
-// 	os.MkdirAll("logs", os.ModePerm)
-
-// 	// Access log file
-// 	accessLog := &lumberjack.Logger{
-// 		Filename:   "logs/access.log",
-// 		MaxSize:    10, // MB
-// 		MaxBackups: 5,
-// 		MaxAge:     30, // hari
-// 		Compress:   true,
-// 	}
-
-// 	// Error log file
-// 	errorLog := &lumberjack.Logger{
-// 		Filename:   "logs/error.log",
-// 		MaxSize:    10,
-// 		MaxBackups: 5,
-// 		MaxAge:     30,
-// 		Compress:   true,
-// 	}
-
-// 	// Set output
-// 	gin.DefaultWriter = io.MultiWriter(accessLog, os.Stdout)
-// 	gin.DefaultErrorWriter = io.MultiWriter(errorLog, os.Stderr)
-// }
