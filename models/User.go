b25396bc52d46abf672ce403a6fbe511445969b2
Aditya <adityapratamax11@gmail.com>
2026-03-30 15:06:36 +0700

    fixing user model

--- models/User.go
+++ models/User.go
@@ -2,16 +2,22 @@ package models
 
 import "time"
 
-type EmailTemplate struct {
-	ID           uint       `gorm:"primaryKey;autoIncrement"`
-	UUID         string     `gorm:"type:char(36);uniqueIndex;not null"`
-	TemplateName string     `gorm:"type:varchar(255);index;not null"`
-	Body         string     `gorm:"type:text"`
-	PrevData     *string    `gorm:"type:text"`
-	CreatedAt    time.Time  `gorm:"autoCreateTime"`
-	UpdatedAt    *time.Time `gorm:"autoUpdateTime"`
-	DeletedAt    *time.Time `gorm:"index"`
-	CreatedBy    *uint
-	UpdatedBy    *uint
-	DeletedBy    *uint
+type User struct {
+	ID            uint       `gorm:"AUTO_INCREMENT" json:"id"`
+	UUID          string     `json:"uuid"`
+	FirstName     string     `json:"first_name"`
+	LastName      string     `json:"last_name"`
+	Username      string     `json:"username"`
+	Email         string     `json:"email"`
+	Phone         string     `json:"phone"`
+	Status        int        `json:"status"`
+	Password      string     `json:"password"`
+	LoginType     string     `json:"login_type"`
+	IDMigration   string     `json:"id_migration"`
+	MigrationDate *time.Time `json:"migration_date"`
+	LastLogin     int64      `json:"last_login"`
+	LastIP        string     `json:"last_ip"`
+	CreatedAt     int64      `json:"created_at"`
+	UpdatedAt     int64      `json:"updated_at"`
+	DeletedAt     int64      `json:"deleted_at"`
 }
